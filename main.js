dcfb91bd225b6ab46c9e225c91ffba04a5c83028
Abdullah_assoli <abdullahassoli@gmail.com>
2022-03-08 07:12:18 +0200

    my work

---
+++ main.js
@@ -0,0 +1,42 @@
+const canvas= document.querySelector("#draw");
+const ctx= canvas.getContext("2d");
+canvas.height=window.innerHeight;
+canvas.width=window.innerWidth;
+ctx.strokeStyle = "white";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 10;
+let isDown=false;
+let lastX=0;
+let lastY=0;
+let hue=1;
+let direction=true;
+function draw(e){
+    if(isDown===false){return};
+    console.log(e);
+    ctx.beginPath();
+    ctx.strokeStyle =`hsl(${hue},100%,50%)`;
+    hue++;
+    ctx.moveTo(lastX,lastY);
+    ctx.lineTo(e.offsetX,e.offsetY);
+    ctx.stroke();
+    [lastX,lastY]=[e.offsetX,e.offsetY];
+    if(hue>=360){
+        hue=0;
+    }
+    if(ctx.lineWidth>=60 || ctx.lineWidth<9){
+        direction=!direction;
+            }
+            if(direction){
+                ctx.lineWidth++;
+            }else{
+                ctx.lineWidth--;
+            }
+        }
+canvas.addEventListener("mousemove",draw);
+canvas.addEventListener("mousedown",function(e){
+    isDown=true;
+    [lastX,lastY]=[e.offsetX,e.offsetY];
+});
+canvas.addEventListener("mouseup",()=>isDown=false);
+canvas.addEventListener("mouseout",()=>isDown=false);
